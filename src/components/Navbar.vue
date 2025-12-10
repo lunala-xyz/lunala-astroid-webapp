9158a347edf78e2ed9fff9caba87d603667fcd3d
Yannick <vankerckvoordeyannick@gmail.com>
2025-12-10 15:29:24 +0100

    Created navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from '@/components/ui/navigation-menu'
+</script>
+
+
+<template>
+  <NavigationMenu>
+    <NavigationMenuList>
+      <NavigationMenuItem>
+        <NavigationMenuLink href="/">
+          Home
+        </NavigationMenuLink>
+      </NavigationMenuItem>
+      <NavigationMenuItem>
+        <NavigationMenuLink href="/about">
+          About
+        </NavigationMenuLink>
+      </NavigationMenuItem>
+      <NavigationMenuItem>
+        <NavigationMenuLink href="https://github.com/Lunala-Studio/Lunala-Astro">
+          Source
+        </NavigationMenuLink>
+      </NavigationMenuItem>
+    </NavigationMenuList>
+  </NavigationMenu>
+</template>
