e6ab6f170ff9b5c12befb0e6578992fa0e0aae07
Yannick <vankerckvoordeyannick@gmail.com>
2025-12-15 10:00:21 +0100

    Updated naming to project and button to project page

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,12 +8,12 @@ import { glob, file } from 'astro/loaders';
 import { z } from 'astro/zod';
 
 // 4. Define your collection(s)
-const blog = defineCollection({
-    loader: glob({ pattern: "**/*.md", base: "./src/data/blogs" }),
+const project = defineCollection({
+    loader: glob({ pattern: "**/*.md", base: "./src/data/projects" }),
     schema: z.object({
         title: z.string(),
     })
 });
 
 // 5. Export a single `collections` object to register your collection(s)
-export const collections = { blog };
+export const collections = { project: project };
