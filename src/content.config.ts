59ed7209f311cb933d140efe43b8e81d66504fe5
Yannick <vankerckvoordeyannick@gmail.com>
2025-12-15 09:29:24 +0100

    Updated content config

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,7 +8,12 @@ import { glob, file } from 'astro/loaders';
 import { z } from 'astro/zod';
 
 // 4. Define your collection(s)
-const blog = defineCollection({ /* ... */ });
+const blog = defineCollection({
+    loader: glob({ pattern: "**/*.md", base: "./src/data/blogs" }),
+    schema: z.object({
+        title: z.string(),
+    })
+});
 
 // 5. Export a single `collections` object to register your collection(s)
 export const collections = { blog };
