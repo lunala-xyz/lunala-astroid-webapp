02c3b6ca1c340ebfc92141461172eebda956f8cc
Yannick <vankerckvoordeyannick@gmail.com>
2025-12-16 14:44:27 +0100

    Split avatar with tooltip to seperate component

---
+++ src/components/AvatarWithTooltip.vue
@@ -0,0 +1,28 @@
+﻿<script setup lang="ts">
+import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@/components/ui/tooltip";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
+interface Props { member: TeamMember }
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <TooltipProvider>
+    <Tooltip :delay-duration="100">
+      <TooltipTrigger as-child>
+        <a :href="`https://github.com/${props.member.username}`" target="_blank">
+          <Avatar>
+            <AvatarImage :src="'https://github.com/' + props.member.username + '.png'" :alt="props.member.username"/>
+            <AvatarFallback>{{ props.member.fallback }}</AvatarFallback>
+          </Avatar>
+        </a>
+      </TooltipTrigger>
+      <TooltipContent>
+        {{ props.member.username }}
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/TeamAvatars.vue
+++ src/components/TeamAvatars.vue
@@ -1,6 +1,5 @@
 ﻿<script setup lang="ts">
-import {Avatar, AvatarFallback, AvatarImage} from '@/components/ui/avatar'
-import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger,} from '@/components/ui/tooltip'
+import AvatarWithTooltip from "@/components/AvatarWithTooltip.vue";
 
 const props = defineProps<{
   teamMembers: Array<TeamMember>,
@@ -8,23 +7,9 @@ const props = defineProps<{
 </script>
 
 <template>
-  <TooltipProvider>
     <div class="flex flex-row flex-wrap items-center gap-4 mt-1 mb-3">
       <div v-for="member in props.teamMembers" :key=member.username>
-        <Tooltip :delay-duration="100">
-          <TooltipTrigger as-child>
-            <a :href="`https://github.com/${member.username}`" target="_blank">
-              <Avatar>
-                <AvatarImage :src="'https://github.com/' + member.username + '.png'" :alt="member.username"/>
-                <AvatarFallback>{{ member.fallback }}</AvatarFallback>
-              </Avatar>
-            </a>
-          </TooltipTrigger>
-          <TooltipContent>
-            {{ member.username }}
-          </TooltipContent>
-        </Tooltip>
+       <AvatarWithTooltip :member="member" />
       </div>
     </div>
-  </TooltipProvider>
 </template>
