45859bd5662e58b54d713f9272f91977d384dace
Yannick <vankerckvoordeyannick@gmail.com>
2025-12-16 14:50:04 +0100

    Fixed tooltip staying after avatar was clicked

--- src/components/AvatarWithTooltip.vue
+++ src/components/AvatarWithTooltip.vue
@@ -1,15 +1,25 @@
 ﻿<script setup lang="ts">
+import { ref } from 'vue';
 import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@/components/ui/tooltip";
 import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
 interface Props { member: TeamMember }
 const props = defineProps<Props>()
+
+const open = ref(false);
+
+// Block the tooltip from staying open after clicking the link
+function handleClick(e: MouseEvent) {
+  open.value = false;
+  const element = e.currentTarget as HTMLElement | null;
+  element?.blur();
+}
 </script>
 
 <template>
   <TooltipProvider>
-    <Tooltip :delay-duration="100">
+    <Tooltip v-model:open="open" :delay-duration="100">
       <TooltipTrigger as-child>
-        <a :href="`https://github.com/${props.member.username}`" target="_blank">
+        <a :href="`https://github.com/${props.member.username}`" target="_blank" @click="handleClick">
           <Avatar>
             <AvatarImage :src="'https://github.com/' + props.member.username + '.png'" :alt="props.member.username"/>
             <AvatarFallback>{{ props.member.fallback }}</AvatarFallback>
